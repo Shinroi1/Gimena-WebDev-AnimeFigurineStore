abc79b636ae9e6b69d3d2b02f72c87ca8e23137c
Shinroi1 <145971744+Shinroi1@users.noreply.github.com>
2023-12-15 18:03:28 +0800

    Create database.php

---
+++ Week 5/Database/database.php
@@ -0,0 +1,6 @@
+<?php
+$db_host = 'localhost'; // Host name
+$db_name = 'animefigurineshop'; // Database name
+$db_username = 'root'; // Database username
+$db_password = ''; // Database password
+?>
