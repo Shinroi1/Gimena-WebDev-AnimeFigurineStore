1a845ec81a4bc2d6ac61b0b3658dc7ff9895782a
Shinroi1 <145971744+Shinroi1@users.noreply.github.com>
2023-12-15 18:18:39 +0800

    Create Menu.php

---
+++ Week 5/Pages/Menu.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anime Figurine Shop</title>
+    <link rel="stylesheet" href="../CSS/Menu.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>🛒ANIME FIGURINES SHOP 🛒</h1>
+
+        <div class = "center">
+
+        <?php include'../Users/Profile.php'; ?>
+
+        <ol>
+            <li><a href="Show.php">Show anime figurines orders</a></li> 
+            <li><a href="Retrieve.html">Retrieve an anime figurines order</a></li>
+            <li><a href="Insert.html">Insert a new anime figurines order</a></li>
+            <li><a href="Update.html">Update an existing anime figurines order</a></li>
+            <li><a href="Delete.html">Delete an anime figurines order</a></li>
+            <li><a href="../Users/Logout.php">Logout</a></li>
+        </ol>
+    </div>
+    </div>
+
+    
+</body>
+
+</html>
+
